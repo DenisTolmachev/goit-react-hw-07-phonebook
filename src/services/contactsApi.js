234136b2303c7f389addbe8d6bcd0d7cd810542d
Denis <denis.tolmachev@gmail.com>
2022-08-20 13:14:45 +0300

    add api functions addContact & deleteContact

--- src/services/contactsApi.js
+++ src/services/contactsApi.js
@@ -1,8 +1,17 @@
-import axios from "axios";
+import axios from 'axios';
 
 axios.defaults.baseURL = 'https://62f8f320e0564480352fc15c.mockapi.io';
 
 export async function fetchContacts() {
-    const {data} = await axios.get(`/contacts`)
-    return data;
+  const { data } = await axios.get(`/contacts`);
+  return data;
+}
+
+export async function addContact(contact) {
+  const { data } = await axios.post(`/contacts`, contact);
+  return data;
+}
+
+export async function deleteContact(id) {
+    await axios.delete(`/contacts/${id}`);
 }
