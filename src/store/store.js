2a3a6ff6226dd7d7b6b6062f005d8a745b7843bb
Denis <denis.tolmachev@gmail.com>
2022-08-21 17:20:20 +0300

    delete persist

--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import contacts from './contacts/contacts';
-import { persistStore } from 'redux-persist';
-import { middleware } from './middleware';
 
 export const store = configureStore({
   reducer: {
     contacts,
   },
-  middleware,
 });
 
-export const persistor = persistStore(store);
