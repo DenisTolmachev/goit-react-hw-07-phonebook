f85ba7586195a1a2172fb1a3a6df468a292a3fa2
Denis <denis.tolmachev@gmail.com>
2022-08-23 19:58:18 +0300

    correct api and redusers

--- src/services/contactsApi.js
+++ src/services/contactsApi.js
@@ -4,13 +4,13 @@ import { BASE_URL } from 'constants/constants';
 axios.defaults.baseURL = BASE_URL;
 
 export const getContacts = async () => {
-  return await axios.get(`/contacts`);
+  return await axios.get(`/contacts`).then(response => response.data);
 };
 
 export const addContact = async contact => {
-  return await axios.post(`/contacts`, contact);
+  return await axios.post(`/contacts`, contact).then(response => response.data);
 };
 
 export const deleteContact = async id => {
-  return await axios.delete(`/contacts/${id}`);
+  return await axios.delete(`/contacts/${id}`).then(() => id);
 };
--- src/store/contacts/contactsOperations.js
+++ src/store/contacts/contactsOperations.js
@@ -4,40 +4,40 @@ import { toast } from 'react-toastify';
 
 export const getCurrentContacts = createAsyncThunk(
   'contacts/getCurrentContacts',
-  async (_, { rejectWithValue }) => {
+  async () => {
     try {
       const data = await API.getContacts();
       return data;
     } catch (error) {
-      return rejectWithValue(error);
+      return toast.error(error);
     }
   }
 );
 
 export const addNewContact = createAsyncThunk(
   'contacts/addNewContact',
-  async (contact, { rejectWithValue }) => {
+  async (contact) => {
     try {
       await API.addContact(contact);
       toast.success('Contact added!');
       const data = await API.getContacts();
       return data;
     } catch (error) {
-      return rejectWithValue(error);
+      return toast.error(error);
     }
   }
 );
 
 export const deleteCurrentContact = createAsyncThunk(
   'contacts/deleteCurrentContact',
-  async (id, { rejectWithValue }) => {
+  async (id) => {
     try {
       await API.deleteContact(id);
       toast.success('Contact deleted!');
       const data = await API.getContacts();
       return data;
     } catch (error) {
-      return rejectWithValue(error);
+      return toast.error(error);
     }
   }
 );
