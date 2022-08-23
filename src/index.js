fb6d148b13b44fe0d94338818bb904066308370a
Denis <denis.tolmachev@gmail.com>
2022-08-23 15:43:57 +0300

    add style dir

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'App';
 import { ThemeProvider } from 'styled-components';
-import { theme } from 'theme';
+import { theme } from 'style/theme';
 import { store } from 'store/store';
 import { Provider } from 'react-redux';
-import GlobalCSS from './global.css';
+import GlobalCSS from './style/global.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
